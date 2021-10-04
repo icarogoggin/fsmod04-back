18018cf12bae1526c642646306887ea1fc7e6368
icarogoggin <icaro.goggin@gmail.com>
2021-10-04 16:24:48 -0300

    Refazendo estrutura do bd

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GamesModule } from './games/games.module';
+import { GenresModule } from './genres/genres.module';
+import { ProfilesModule } from './profiles/profiles.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [GamesModule, GenresModule, ProfilesModule, UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
