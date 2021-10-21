06fb54ab4817a38905759ad538d7d67ae0326f5d
icarogoggin <icaro.goggin@gmail.com>
2021-10-20 23:38:18 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { UnauthorizedInterceptor } from './interceptors/unauthorized.interceptor
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: { origin: 'http://localhost:3001' },
+    cors: { origin: ['http://localhost:3001', 'https://fsmod04-back.herokuapp.com/'] },
   });
   app.useGlobalPipes(
     new ValidationPipe({
